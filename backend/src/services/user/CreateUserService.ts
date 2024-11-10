f8798c1dc3be6fb233014d9c85a7a24d2db65853
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-11-10 17:35:53 -0300

    feat: Adiciona bcryptjs e implementa criptografia de senha
    - Instala a biblioteca bcryptjs para criptografia segura de senhas
    - Implementa a função de hash da senha antes de salvar no banco de dados
    - Atualiza o modelo de usuário para usar a senha criptografada

--- backend/src/services/user/CreateUserService.ts
+++ backend/src/services/user/CreateUserService.ts
@@ -1,4 +1,5 @@
 import prismaClient from "../../prisma";
+import { hash } from "bcryptjs"
 
 interface UserRequest {
     name: string;
@@ -26,11 +27,13 @@ class CreateUserService{
             throw new Error("É obrigatório o usuário enviar uma senha!")
         }
 
+        const passwordHash = await hash(password, 8)
+
         const user = await prismaClient.user.create({
             data: {
                 name,
                 email,
-                password
+                password: passwordHash
             },
             select: {
                 id: true,
