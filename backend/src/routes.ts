5eb951778877002b61e5cddbf2d29f82d6cdc190
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-11-12 10:35:14 -0300

    feat: Adiciona rota inicial para cadastro de produtos

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -5,19 +5,23 @@ import { DetailUserController } from "./controllers/user/DetailUserController";
 import { isAuthenticated } from "./middlewares/isAuthenticated";
 import { CreateCategoryController } from "./controllers/category/CreateCategoryController";
 import { ListCategoryController } from "./controllers/category/ListCategoryController";
+import { CreateProductController } from "./controllers/product/CreateProductController";
 
 const router = Router();
 
-//Instâncias das controllers relacionadas à criação, autenticação e detalhe de usuário.
+//Instâncias das controllers relacionadas à criação, autenticação e detalhes de usuário.
 const createUserController = new CreateUserController();
 const authUserController = new AuthUserController()
 const detailUserController = new DetailUserController()
 
-//Instâncias de controllers relacionadas à a criação de categorias de produtos.
+//Instâncias de controllers relacionadas à criação e a detalhes das categorias dos produtos.
 const createCategoryController = new CreateCategoryController()
 const listCategoryController = new ListCategoryController()
  
-//Rotas relacionado à criação, autenticação e detalhes do usuário.
+//Instâncias de controllers relacionadas a criação e detalhes dos produtos.
+const createProductController = new CreateProductController()
+
+//Rotas relacionadas à criação, autenticação e detalhes do usuário.
 router.post("/users", async (req: Request, res: Response) => {
   await createUserController.handle(req, res);
 });
@@ -30,7 +34,7 @@ router.get("/me", isAuthenticated, async (req, res) => {
   await detailUserController.handle(req, res);
 })
 
-//Rotas relacionado à criação de categorias.
+//Rotas relacionadas à criação e detalhes das categorias dos produtos.
 router.post("/category",isAuthenticated  ,async (req: Request, res: Response) => {
   await createCategoryController.handle(req, res);
 })
@@ -38,5 +42,10 @@ router.post("/category",isAuthenticated  ,async (req: Request, res: Response) =>
 router.get("/category", isAuthenticated, async (req: Request, res: Response) => {
   await listCategoryController.handle(req, res);
 })
+
+//Rotas relacionadas a criação e detalhes das categorias.
+router.post("/product", isAuthenticated, async (req: Request, res: Response) => {
+  await createProductController.handle(req, res);
+}) 
 
 export { router } 
