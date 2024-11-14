779818c5ea155ad956958d0d1efc931f0785ff8e
Leonardo Nunes <Leonardomartinha.dev@gmail.com>
2024-11-14 10:15:53 -0300

    feat(styles): implementa configuração do CSS global
    - Define variáveis de cores e fontes
    - Estabelece estilos base para elementos HTML comuns
    - Inclui classes utilitárias para flexibilidade no layout

--- frontend/tailwind.config.ts
+++ frontend/tailwind.config.ts
@@ -6,6 +6,26 @@ export default {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
-  theme: {},
+  theme: {
+    extend: {
+      colors: {
+        brand: {
+          dark: {
+            900: "#101026",
+            800: "#1D1D2E",
+          },
+          gray: {
+            100: "#8A8A8A"
+          },
+          green: {
+            100: "#3FFFA3"
+          },
+          red: {
+            100: "#FF3F4B"
+          }
+        }
+      }
+    }
+  },
   plugins: [],
 } satisfies Config;
